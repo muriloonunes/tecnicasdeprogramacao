4810dc71f90783c63c92a857c08bd431dcb7adbf
Murilo <murilo_no@outlook.com>
2024-09-28 16:16:34 -0300

    Ex3

---
+++ src/main/java/aula14/Ex3/application/Programa.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024. Murilo Nunes <murilo_no@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package aula14.Ex3.application;
+
+import aula14.Ex3.entities.Comentario;
+import aula14.Ex3.entities.Post;
+
+/**
+ * @author Murilo Nunes <murilo_no@outlook.com>
+ * @date 28/09/2024
+ * @brief Class Programa.
+ */
+public class Programa {
+    public static void main(String[] args) {
+        Post post1 = new Post("2024-09-27T19:23:30","Viajando para a Nova Zelandia",
+                "Estou indo visitar esse lindo pais!", 12);
+
+        Comentario c1 = new Comentario("Faça uma boa viagem!");
+        Comentario c2 = new Comentario("Nossa que incrivel");
+
+        post1.adicionarComentario(c1);
+        post1.adicionarComentario(c2);
+
+        Post post2 = new Post("2024-09-28T16:15:58","Boa noite gnt",
+                "Vejo vcs amanha", 5);
+
+        Comentario c3 = new Comentario("Boa noite");
+        Comentario c4 = new Comentario("Que a Força esteja com voce");
+        post2.adicionarComentario(c3);
+        post2.adicionarComentario(c4);
+
+        System.out.println(post1);
+        System.out.println(post2);
+    }
+}
---
+++ src/main/java/aula14/Ex3/entities/Post.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024. Murilo Nunes <murilo_no@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package aula14.Ex3.entities;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+/**
+ * @author Murilo Nunes <murilo_no@outlook.com>
+ * @date 28/09/2024
+ * @brief Class Post.
+ */
+public class Post {
+    private LocalDateTime momento;
+    private String titulo;
+    private String conteudo;
+    private int likes;
+    private ArrayList<Comentario> comentarios = new ArrayList<>();
+
+    public Post(String momentoString, String titulo, String conteudo, int likes) {
+        this.momento = LocalDateTime.parse(momentoString, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.likes = likes;
+    }
+
+    public void adicionarComentario(Comentario comentario) {
+        comentarios.add(comentario);
+    }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder resultado = new StringBuilder(titulo + "\n"
+                + likes + " Likes - "
+                + momento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) + "\n"
+                + conteudo + "\n"
+                + "Comments: \n");
+
+        for (Comentario comentario : comentarios) {
+            resultado.append(comentario.getComentario()).append("\n");
+        }
+        return resultado.toString();
+    }
+
+}
